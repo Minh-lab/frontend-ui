1dc0895a02714dd486934efd765fa3f75e479af2
Minh Nguyen <minhn6673@gmail.com>
2026-03-09 18:46:11 +0700

    Revert "Giao dien doanh nghiep"

--- src/pages/businesses/ProfilePageBusiness.jsx
+++ src/pages/businesses/ProfilePageBusiness.jsx
@@ -4,7 +4,6 @@ import { Card } from '@/components/ui/card'
 import { business } from '@/data/businessData'
 import { User } from 'lucide-react'
 import React, { useState } from 'react'
-import { toast } from 'sonner'
 
 function Field({ label = "sd", value }) {
   return (
@@ -51,7 +50,7 @@ const ProfilePageBusiness = () => {
         </div>
 
         {/* Body */}
-        <div className="p-6 space-y-4 bg-[#FCFCFC]">
+        <div className="p-6 space-y-4">
 
           <Field label="Mã số thuế:" value={business.maST} />
           <Field label="Tên doanh nghiệp:" value={business.tenDN} />
@@ -75,10 +74,10 @@ const ProfilePageBusiness = () => {
                             ))}
                             <div className="flex justify-end gap-3 pt-2">
                               <button onClick={() => setShowPwC(false)} className="px-5 py-2.5 text-sm border border-gray-300 text-gray-600 rounded-lg hover:bg-gray-50 transition">Huy</button>
-                              <button onClick = {() => {setShowPwC(false); toast.success("Hành động của bạn đã được ghi nhận",{className: "!bg-[#AAFAB8] !text-[#24AD47]"})}} className="px-5 py-2.5 text-sm bg-[#5c60c0] text-white rounded-lg hover:bg-[#4a4ea8] font-medium transition">Xac nhan</button>
+                              <button onClick={handleChangePassword} className="px-5 py-2.5 text-sm bg-[#5c60c0] text-white rounded-lg hover:bg-[#4a4ea8] font-medium transition">Xac nhan</button>
                             </div>
                           </div>
-                  </Modal>
+                        </Modal>
               )}
 
         
--- src/pages/businesses/QuanLyThucTap.jsx
+++ src/pages/businesses/QuanLyThucTap.jsx
@@ -1,37 +1,20 @@
-import Modal from '@/components/Modal'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { sinhvienTT } from '@/data/businessData'
-import React, { useState } from 'react'
-import tlu from "../../assets/logo-tlu.png"
-import { toast } from 'sonner'
-function PreviewBaoCao() {
-  return (
-    <Card className=" bg-white text-center ">
-      <p className="text-xs font-semibold tracking-wide">BỘ GIÁO DỤC VÀ ĐÀO TẠO &nbsp;&nbsp;&nbsp; BỘ NÔNG NGHIỆP VÀ PTNT</p>
-      <p className="text-xs font-semibold  tracking-wide">TRƯỜNG ĐẠI HỌC THỦY LỢI</p>
-      <div className="w-20 h-20 flex items-center justify-center mx-auto w-[150px]">
-          <img src={tlu} alt="" />  
-      </div>
-      <p className="text-xs font-semibold text-blue-700 tracking-widest mt-1">BÁO CÁO THỰC TẬP</p>
-    </Card>
-  );
-}
+import React from 'react'
 
 const QuanLyThucTap = () => {
-  const [selectedSv, setSelectedSv] = useState(null)
-
   return (
       <div className='p-6'>
         <Card className="bg-[#ECF9FF]">
-          <span className = "text-center text-[25px] font-inter text-[#445298] font-bold">Chấm điểm và đánh giá</span>
+          <span className = "text-center text-[25px] font-inter text-[#445298] font-bold">Xác nhận thực tập</span>
           <div className='px-6'>
             <span className='font-inter'>Danh sách sinh viên đăng ký thực tập</span>
           </div>
           <Card className="mx-6 overflow-hidden p-0">
             <table className='text-x font-normal'>
               <thead >
-                <tr className='bg-gray-50 border-b border-gray-200'>
+                <tr className='bg-gray-50 border-b border-gray-100'>
                   <th className="px-4 py-2.5 text-left font-normal font-inter">
                     STT
                   </th >
@@ -57,7 +40,7 @@ const QuanLyThucTap = () => {
               </thead>
               <tbody>
                   {sinhvienTT.map((sv) => (
-                      <tr key = {sv.id} className='bg-white border-b border-gray-200 hover:bg-gray-50/50 transition'>
+                      <tr key = {sv.id} className='bg-white border-b border-gray-50 hover:bg-gray-50/50 transition'>
                         <td className='px-4 py-3  font-inter text-gray-700'>
                           {sv.id}
                         </td>
@@ -80,7 +63,7 @@ const QuanLyThucTap = () => {
                             className={`px-3 py-1 rounded-full text-sm font-medium ${
                               sv.trangthai === "chua danh gia"
                                 ? "bg-[#FFF2D2] text-[#DA5C02] border border-[#DA5C02]"
-                                : "bg-[#BCFFC2] text-[#24AD47]border border-[#24AD47]"
+                                : "bg-[#BCFFC2] text-green-700 border border-[#24AD47]"
                             }`}
                           >
                             {sv.trangthai}
@@ -88,7 +71,7 @@ const QuanLyThucTap = () => {
                         </td>
                         <td className='px-4 py-3'>
                             <div className='flex gap-5'>
-                              <Button onClick = {() => setSelectedSv(sv)} className="">
+                              <Button className="">
                                 Xem chi tiết
                               </Button>
   
@@ -100,42 +83,6 @@ const QuanLyThucTap = () => {
               </tbody>
             </table>
           </Card>
-          {selectedSv  && (
-            <Modal size = 'xl' className = "" title = {"Chấm điểm và đánh giá"} onClose={() => setSelectedSv(null)}>
-                <div className='bg-[#ECF9FF] -m-6 p-6 min-h-full'>
-                  <span>Thông tin sinh viên: </span>
-                  <div className="grid grid-cols-1 md:grid-cols-3 gap-x-2">
-                    <p><span className="font-semibold">Mã sinh viên:</span> {selectedSv.maSV}</p>
-                    <p><span className="font-semibold">Họ và tên:</span> {selectedSv.hoTen}</p>
-                    <p><span className="font-semibold">Vị trí thực tập:</span> {selectedSv.vitri}</p>
-
-                    <p><span className="font-semibold">Email:</span> {selectedSv.email}</p>
-                    <p><span className="font-semibold">SDT:</span> {selectedSv.soDT}</p>
-                    <p><span className="font-semibold">Trường:</span> Đại học Thủy Lợi</p>
-                  </div><br />
-          
-                  <span>Preview bao cao:</span>
-
-                  <PreviewBaoCao /> <br />
-                  
-                  <div className='flex gap-3 items-center'>
-                    <span className='font-bold p-1'>Diem doanh nghiep cham: </span>
-                    <input type="number" step= "0.25" className='bg-[#DBF7E4] text-[#0FB245] flex-1 border border-gray-400 p-1 h-[50px]'/>
-                  </div><br />
-
-                  <span className='font-bold'>Nhan xet chi tiet:</span>
-                  <div className='flex'>
-                    <textarea className='flex-1 bg-[#FCFCFC] border border-gray-500 p-1 h-[80px]'/>
-                  </div>
-                  
-                    <div className='flex gap-4 pt-4 justify-end'>
-                      <Button onClick = {() => setSelectedSv(null) } className="bg-[#FF0000] hover:bg-[#cc0000] text-white w-[60px] h-9 p-0">Huy</Button>
-                      <Button onClick = {() => {setSelectedSv(null); toast.success("Hành động của bạn đã được ghi nhận",{className: "!bg-[#AAFAB8] !text-[#24AD47]"})}} className="bg-[#24AD47] hover:bg-[#1f933d] text-white w-[80px] h-9 p-0">Luu</Button>
-                    </div>
-                  
-                </div>
-            </Modal>
-          )}
         </Card>
       </div>
     )
